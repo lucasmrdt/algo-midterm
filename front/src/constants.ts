f2c940d95b6a9ca9a3f2c33ec53074a0169f3ca4
Lucas Marandat <lucas.mrdt@gmail.com>
2021-11-03 17:12:17 +0100

    update endpoints

--- front/src/constants.ts
+++ front/src/constants.ts
@@ -1,1 +1,4 @@
-export const ENDPOINT = "http://localhost:5000";
+export const ENDPOINT =
+  process.env.NODE_ENV === "production"
+    ? "https://algo-midterm.herokuapp.com"
+    : "http://localhost:5000";
