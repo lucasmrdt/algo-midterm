43e67b06184652efb1301c45bc362a3eb1571029
Lucas Marandat <lucas.mrdt@gmail.com>
2021-11-04 22:09:07 +0100

    fix heroku env variable parsing

--- back/rflask/constants.py
+++ back/rflask/constants.py
@@ -1,5 +1,9 @@
 import os
+import re
 
 SQLALCHEMY_DATABASE_URI = os.environ.get(
     "DATABASE_URL", "sqlite:///sqlitedb.file")
+SQLALCHEMY_DATABASE_URI = re.sub(
+    r"^postgres://", "postgresql://", SQLALCHEMY_DATABASE_URI)
+
 CSV_FILE_PATH = os.environ.get("CSV_FILE", "data.csv")
